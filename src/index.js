a3a67809c05ec2a51afd121e54a4dcfec4f73a10
Kate Lupashtsian <kate.lupashtsian@gmail.com>
2020-10-18 15:54:01 +0300

    feat: implemented function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = [];
+  if (matrix === undefined || matrix.length === 0){
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) {
+      newArr = newArr + ',' + matrix[i].reverse();
+     
+    } else {
+      newArr = newArr + ',' + matrix[i];
+       
+    } 
+  }
+  let oneStr = newArr.split(',');
+  let newN = oneStr.map(Number);
+  newN.splice(0, 1);
+  return newN;
 }
